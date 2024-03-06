1fc221bf99adffebf89399267435f5321e02b12a
deepak-Ui/Ux <deepak.mpcz5@gmail.com>
2024-03-06 16:30:02 +0530

    new project setup

---
+++ assest/js/styleswitcher.js
@@ -0,0 +1,30 @@
+
+
+
+const links document.querySelectorAll(".alternate-style"),
+totalLinks links.length; 
+
+function setActiveStyle(coLor)
+{
+	for(let i=0; i totalLinks; i++)
+	{
+		(color== links[i].getAttribute("title"))
+		{
+			links[i].removeAttribute("disabled"); 
+		} 
+	}
+	else
+	{
+		links[i].setAttribute("disabled","true");
+	} 
+}
+
+
+
+
+ 
+ 
+
+
+
+
